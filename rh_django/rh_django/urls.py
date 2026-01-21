54db913dc24990d011544738ed6c819a39a8882b
Mildreth-SC <e1314433382@live.uleam.edu.ec>
2026-01-21 03:59:02 -0500

    Add diagnostics endpoint

--- rh_django/rh_django/urls.py
+++ rh_django/rh_django/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from django.conf import settings
 
 def home(request):
     return JsonResponse({
@@ -13,8 +14,17 @@ def home(request):
         }
     })
 
+def diagnostics(request):
+    return JsonResponse({
+        'host': request.get_host(),
+        'allowed_hosts': settings.ALLOWED_HOSTS,
+        'debug': settings.DEBUG,
+        'headers': dict(request.headers),
+    })
+
 urlpatterns = [
     path('', home, name='home'),
+    path('diagnostics/', diagnostics, name='diagnostics'),
     path('admin/', admin.site.urls),
     path('api/', include('empleados.urls')),
 ]
